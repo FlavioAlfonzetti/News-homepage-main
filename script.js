48fbfa44f4703ceafb6b15f8b1688cfb788106d5
Flavio Alfonzetti <flavioalfonzetti@gmail.com>
2023-02-04 00:02:44 +0100

    second commit

---
+++ script.js
@@ -0,0 +1,13 @@
+/* function openNav() {
+    document.getElementById("sidebar").style.transform = "translateX(0)";
+    document.getElementById("overlay").style.opacity = "0.6";
+    document.getElementById("overlay").style.visibility = "visible"; 
+    document.getElementById("overlay").style.width = "100%";
+  }
+
+  function closeNav() {
+    document.getElementById("sidebar").style.transform = "translateX(100%)";
+    document.getElementById("overlay").style.opacity = "0";
+    document.getElementById("overlay").style.visibility = "hidden"; 
+    document.getElementById("overlay").style.width = "0";
+  } */
